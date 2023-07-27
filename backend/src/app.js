83b620a1b0f2879390db15ed3e00eb522d8b69df
Anderson Andrade <andrade.28me@gmail.com>
2023-07-27 09:11:32 -0300

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,9 @@
 const express = require('express'); //importar o express
 const router = require('./router') //mesmo que import
-
+const cors = require('cors')
 const app = express(); //criando app
-app.use(express.json())
-app.use(router)
+app.use(cors());
+app.use(express.json());
+app.use(router);
 
 module.exports = app;
